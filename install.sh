be556029e8d307d71c78c02c6d79351ad8bde258
kareza <kareza@qq.com>
2022-01-03 19:22:49 +0800

    增加备份zshrc与替换zshrc功能

--- install.sh
+++ install.sh
@@ -11,3 +11,8 @@ mv ~/.vim $backup/.vim
 mv ~/.vimrc $backup/.vimrc
 ln -s ~/.dotfiles/.vimrc ~/.vimrc
 ln -s ~/.ditfiles/.vim ~/.vim
+
+## zsh
+
+mv ~/.zshrc $backup/.zshrc
+ln -s ~/.dotfiles/.zshrc ~/.zshrc
